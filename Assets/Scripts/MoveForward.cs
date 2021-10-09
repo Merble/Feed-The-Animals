e74cbaaeccfcbb64f931b52e4fe971f8cc1354cc
Ömer Tekin <tekinomer241@gmail.com>
2021-10-09 16:53:21 +0300

    First Upload
    First Upload

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 30.0f;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if(transform.position.z > 35 || transform.position.z < -20)
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] animalPrefabs;
+    [SerializeField]
+    private float xRange = 20.0f;
+    [SerializeField]
+    private float zPosition = 30.0f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange + 1), 0, zPosition);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    } 
+}
